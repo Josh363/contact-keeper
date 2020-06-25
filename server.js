d04a174cf39adbd0ede3c6cac8c708a34873b4b6
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-06-25 15:39:41 -0500

    fixed build directory

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-//Serve static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //Set static folder
+  // Set static folder
   app.use(express.static('client/build'))
-  //grab all routes, except those above (client routes)
+
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
 }
 
